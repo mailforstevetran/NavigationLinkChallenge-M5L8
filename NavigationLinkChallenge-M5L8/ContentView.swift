b1211f8ff65f3e922860075bad26edd8f8def780
Steve Tran <mailforstevetran@gmail.com>
2021-06-23 10:06:33 +0900

    Initial Commit

---
+++ NavigationLinkChallenge-M5L8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationLinkChallenge-M5L8
+//
+//  Created by Steve Tran on 23/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationLinkChallenge-M5L8/NavigationLinkChallenge_M5L8App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLinkChallenge_M5L8App.swift
+//  NavigationLinkChallenge-M5L8
+//
+//  Created by Steve Tran on 23/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLinkChallenge_M5L8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
